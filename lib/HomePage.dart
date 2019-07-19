4d5e3d1dd3ea0e524a3e9db708d402f350724f2c
ankit <ankitgupta_@outlook.com>
2019-07-19 20:48:16 +0530

    firebase

---
+++ lib/HomePage.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:akss/Screens/addContact.dart';
+import 'package:akss/Screens/view_contact.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_database/ui/firebase_animated_list.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+ 
+ DatabaseReference _databaseReference = FirebaseDatabase.instance.reference();
+ 
+ navigateToAddScreen(){
+   Navigator.push(context, MaterialPageRoute(builder: (context){
+     return AddContact();
+   }));
+ }
+
+ navigateToViewScreen(id){
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+     return ViewContact(id);
+ }));}
+
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Contact App"),
+      ),
+      body: Container(
+        child: FirebaseAnimatedList(
+          query: _databaseReference,
+          itemBuilder: (BuildContext context,DataSnapshot snapshot,Animation<double>
+          animation, int index){
+            return GestureDetector(
+              onTap: (){
+                navigateToViewScreen(snapshot.key);
+              },
+              child: Card(
+                color: Colors.white,
+                elevation: 2.0,
+                child: Container(
+                  margin: EdgeInsets.all(10.0),
+                  child: Row(
+                    children: <Widget>[
+                Container(
+                  width: 50.0,
+                  height: 50.0,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    image:DecorationImage(
+                      fit: BoxFit.cover,
+                      image: snapshot.value['photoUrl'] =='empty'?AssetImage("images/storage.png"):
+                    NetworkImage(snapshot.value["photoUrl"])
+                    ) 
+                  ),
+                ),
+                Container(
+                  margin: EdgeInsets.all(20.0),
+                  child: Column(children: <Widget>[
+                    Text("${snapshot.value['firstName']} ${snapshot.value['lastName']}",
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),),
+                     Text("${snapshot.value['phone']}")
+                  ],),
+                )
+                    ],
+                  ),
+                ),
+              ),
+            );
+          },
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: navigateToAddScreen,),
+    );
+  }
+}
